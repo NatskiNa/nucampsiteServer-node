cff22582d8932e14c16c97c0b73db5affb519192
Your NameNatski <natsukina@gmail.com>
2024-02-15 15:12:44 -0800

    Passport Jwt

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var express = require('express');
 var path = require('path');
 //var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
+//const session = require('express-session');
+//const FileStore = require('session-file-store')(session);
 const passport = require('passport');
-const authenticate = require('./authenticate');
-
+//const authenticate = require('./authenticate');
+const config = require('./config');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
@@ -17,7 +17,7 @@ const partnerRouter = require('./routes/partnerRouter');
 const mongoose = require('mongoose');
 
 //connect to mongoDB server
-const url = 'mongodb://localhost:27017/nucampsite';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
   useFindAndModify: false,
@@ -41,35 +41,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('047-423-5716'));
 
-app.use(
-  session({
-    name: 'session-id',
-    secret: '047-423-5716',
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore(),
-  })
-);
-
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-function auth(req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-    const err = new Error('You are not authenticated!');
-    err.status = 401;
-    return next(err);
-  } else {
-    return next();
-  }
-}
-
-app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  secretKey: '047-423-5716',
+  mongoUrl: 'mongodb://localhost:27017/nucampsite',
+};
